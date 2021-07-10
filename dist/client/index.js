92ad160a483df90c787696cdb17c5a66b383fc20
fortune cookie <767077147@qq.com>
2021-07-10 10:58:00 +0800

    chore: update gitignore

--- dist/client/index.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-console.log(3213132);
--- dist/server/server.d.ts
+++
@@ -1,6 +0,0 @@
-interface CreateServerConfig {
-    port: number;
-    cwd: string;
-}
-export declare const createServer: ({ port, cwd }: CreateServerConfig) => void;
-export {};
